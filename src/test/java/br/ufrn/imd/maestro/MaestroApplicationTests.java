5408d45a26f6a9955b8606679a2bcdf00d55d7ca
Gabrielly Freire <gabylopesfreire@gmail.com>
2026-03-29 20:34:26 -0300

    init: criação da estrutura do projeto spring

---
+++ src/test/java/br/ufrn/imd/maestro/MaestroApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufrn.imd.maestro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaestroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
